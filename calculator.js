4715bc10e7020e1530d832a41c83d25c55414e59
kudryavtsevaa99 <kudryyaaa@yandex.ru>
2024-09-27 15:00:28 +0200

    Refactoring

--- calculator.js
+++ calculator.js
@@ -1,57 +1,74 @@
-const DIGIT_BUTTONS = document.querySelectorAll(".digit");
-
-// const SIGN = document.querySelectorAll(".operation");
-
-const BTN_VALUES = Array.from(document.querySelectorAll(".button")).map(
-  (el) => el.innerHTML
-);
-// console.log(BTN_VALUES);
-
-const keybord = document.querySelector(".keybord");
-const input = document.querySelector("#myInput");
-
-keybord.addEventListener("click", function (event) {
-  const btnValue = event.target.innerHTML;
-  // console.log(btnValue);
-
-  if (!BTN_VALUES.includes(btnValue)) {
-    return;
-  }
-  input.value = input.value + btnValue;
-});
-
-const ALL_CLEAR = document.querySelector(".ac");
-
-function deleteInputValue() {
-  if (input.value) {
-    input.value = "";
-  }
-}
-
-ALL_CLEAR.addEventListener("click", deleteInputValue);
-
-// знак = на экране при нажатии на кнопку сделать ярче
-
-const equalButton = document.querySelector(".equal");
-const screen_equal = document.querySelector(".screen-equal");
-
-equalButton.addEventListener("click", () => {
-  screen_equal.classList.add("active");
-});
-
-// реализация вычислительных методов операндов
-
 let state = {
   currentOperation: null, // это может быть сложение, умножение, вычитание, etc. Ключи: -, + и тд
   immediateResult: 0, // "мгновенный" результат. Используется только для рендеринга, !не для расчетов!
   fixedResult: 0, // используется как опорное значение. На его основе считается immediateResult
   currentOperand: "", // это буфер, хранящий текущее число. Чтоб не плюсовали 1, потом 2, потом 3, а сразу 123
 };
+
 const setState = (key, payload) => {
   state = { ...state, [key]: payload };
   renderImmediateResult();
 };
 
+function setInitialState() {
+  setState("currentOperation", null);
+  setState("immediateResult", 0);
+  setState("fixedResult", 0);
+  setState("currentOperand", "");
+}
+
+const DIGIT_BUTTONS = document.querySelectorAll(".digit");
+// const SIGN = document.querySelectorAll(".operation");
+const BTN_VALUES = Array.from(document.querySelectorAll(".button")).map(
+  (el) => el.innerHTML
+);
+const KEYBOARD = document.querySelector(".keyboard");
+const INPUT = document.querySelector("#myInput");
+const ALL_CLEAR = document.querySelector(".ac");
+const PLUS_BUTTON = document.querySelector(".plus");
+const RESULT_SCREEN = document.querySelector(".screen-number");
+const EQUAL_BUTTON = document.querySelector(".equal");
+const SCREEN_EQUAL = document.querySelector(".screen-equal");
+
+KEYBOARD.addEventListener("click", handleKeyboardClick);
+ALL_CLEAR.addEventListener("click", handleAllClearClick);
+EQUAL_BUTTON.addEventListener("click", handleEqualClick);
+PLUS_BUTTON.addEventListener("click", handlePlusClick);
+DIGIT_BUTTONS.forEach((btn) => btn.addEventListener("click", handleDigitClick));
+
+function handleKeyboardClick(event) {
+  const btnValue = event.target.innerHTML;
+  if (!BTN_VALUES.includes(btnValue) || btnValue === "AC") {
+    return;
+  }
+  INPUT.value = INPUT.value + btnValue;
+}
+
+function handleAllClearClick() {
+  setInitialState();
+  INPUT.value = "";
+}
+
+function handleEqualClick() {
+  SCREEN_EQUAL.classList.add("active");
+}
+
+function handlePlusClick() {
+  setState("currentOperation", "+");
+  setState("fixedResult", parseFloat(state.currentOperand));
+  setState("currentOperand", "");
+}
+
+function handleDigitClick(event) {
+  const digit = event.target.innerHTML;
+  const currentOperand = state.currentOperand;
+  const nextOperand = currentOperand + digit;
+  setState("currentOperand", nextOperand);
+  myCalc.calculate();
+  console.log(state);
+}
+
+// реализация вычислительных методов операндов
 function Calculator() {
   this.methods = {
     "+": (a, b) => a + b,
@@ -71,29 +88,7 @@ function Calculator() {
 
 const myCalc = new Calculator();
 
-const plusBtn = document.querySelector(".plus");
-plusBtn.addEventListener("click", () => {
-  setState("currentOperation", "+");
-  setState("fixedResult", parseFloat(state.currentOperand));
-  setState("currentOperand", "");
-  console.log(state);
-});
-
-DIGIT_BUTTONS.forEach((btn) =>
-  btn.addEventListener("click", (event) => {
-    const digit = event.target.innerHTML;
-
-    const currentOperand = state.currentOperand;
-    const nextOperand = currentOperand + digit;
-    setState("currentOperand", nextOperand);
-    // console.log(nextOperand);
-    myCalc.calculate();
-    console.log(state);
-  })
-);
-
 // рендер результата
-const RESULT_SCREEN = document.querySelector(".screen-number");
 function renderImmediateResult() {
   RESULT_SCREEN.innerHTML = state.immediateResult;
 }
