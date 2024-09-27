ca331dabd15f3e7b876538280c352dc5e42e83ed
kudryavtsevaa99 <kudryyaaa@yandex.ru>
2024-09-27 14:35:23 +0200

    Implement base calculator (handles only 1st operation correctly)

--- calculator.js
+++ calculator.js
@@ -1,4 +1,8 @@
-const BTN_VALUES = Array.from(document.querySelectorAll(".digit")).map(
+const DIGIT_BUTTONS = document.querySelectorAll(".digit");
+
+// const SIGN = document.querySelectorAll(".operation");
+
+const BTN_VALUES = Array.from(document.querySelectorAll(".button")).map(
   (el) => el.innerHTML
 );
 // console.log(BTN_VALUES);
@@ -16,10 +20,7 @@ keybord.addEventListener("click", function (event) {
   input.value = input.value + btnValue;
 });
 
-// логика для знака =
-
 const ac = document.querySelector(".ac");
-// const body = document.body;
 
 function deleteInputValue() {
   if (input.value) {
@@ -37,3 +38,66 @@ const screen_equal = document.querySelector(".screen-equal");
 equalButton.addEventListener("click", () => {
   screen_equal.classList.add("active");
 });
+
+// реализация вычислительных методов операндов
+
+let state = {
+  currentOperation: null, // это может быть сложение, умножение, вычитание, etc. Ключи: -, + и тд
+  immediateResult: 0, // "мгновенный" результат. Используется только для рендеринга, !не для расчетов!
+  fixedResult: 0, // используется как опорное значение. На его основе считается immediateResult
+  currentOperand: "", // это буфер, хранящий текущее число. Чтоб не плюсовали 1, потом 2, потом 3, а сразу 123
+};
+const setState = (key, payload) => {
+  state = { ...state, [key]: payload };
+  renderImmediateResult();
+};
+
+function Calculator() {
+  this.methods = {
+    "+": (a, b) => a + b,
+  };
+
+  this.calculate = () => {
+    const { fixedResult, currentOperand, currentOperation } = state;
+    let nextResult = null;
+    if (!currentOperation) {
+      nextResult = parseFloat(currentOperand);
+    } else {
+      nextResult = fixedResult + parseFloat(currentOperand);
+    }
+    setState("immediateResult", nextResult);
+  };
+}
+
+const myCalc = new Calculator();
+
+const plusBtn = document.querySelector(".plus");
+plusBtn.addEventListener("click", () => {
+  setState("currentOperation", "+");
+  setState("fixedResult", parseFloat(state.currentOperand));
+  setState("currentOperand", "");
+  console.log(state);
+});
+
+DIGIT_BUTTONS.forEach((btn) =>
+  btn.addEventListener("click", (event) => {
+    const digit = event.target.innerHTML;
+
+    const currentOperand = state.currentOperand;
+    const nextOperand = currentOperand + digit;
+    setState("currentOperand", nextOperand);
+    // console.log(nextOperand);
+    myCalc.calculate();
+    console.log(state);
+  })
+);
+
+// рендер результата
+const RESULT_SCREEN = document.querySelector(".screen-number");
+function renderImmediateResult() {
+  RESULT_SCREEN.innerHTML = state.immediateResult;
+}
+
+// задача: выводить операторы и операнды (типа "3 + 2 + 59")
+// const INPUT_SCREEN = document.querySelector("#myInput");
+// function renderInput() {}
