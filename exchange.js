15321480a6a4b19fc99d813bdd29f6f8dfb4b2cd
kudryavtsevaa99 <kudryyaaa@yandex.ru>
2024-11-28 20:01:10 +0100

    create function for updating usd currency to rsd - get data via API - add event listeners

---
+++ exchange.js
@@ -0,0 +1,47 @@
+const apiKey = "46b773a9c3d2494ebf50881e8fad50fa";
+const url = `https://openexchangerates.org/api/latest.json?app_id=${apiKey}&symbols=USD,RSD`;
+
+const AMOUNT_INPUT = document.querySelector(".amount");
+const CONVERTED_AMOUNT = document.querySelector(".converted-amount");
+const CURRENCY1 = document.querySelector(".currency1");
+const CURRENCY2 = document.querySelector(".currency2");
+const SWAP_BUTTON = document.querySelector(".swap-button");
+const CLEAR_BUTTON = document.querySelector(".clear-button");
+
+let usdData;
+let rsdData;
+
+// получаем курс валют через апи
+fetch(url)
+  .then((response) => response.json())
+  .then((data) => {
+    usdData = data.rates.USD;
+    rsdData = data.rates.RSD;
+    console.log("Курсы валют получены:", { USD: usdData, RSD: rsdData });
+  })
+  .catch((error) => console.error("Error fetching exchange rates:", error));
+
+// функция для обновления конверт суммы
+function updateConvertedAmount() {
+  const amountValue = parseFloat(AMOUNT_INPUT.value);
+  if (isNaN(amountValue)) {
+    CONVERTED_AMOUNT.value = `0 ${CURRENCY2.value}`;
+    return;
+  }
+
+  let rate;
+  if (CURRENCY1.value === "USD" && CURRENCY2.value === "RSD") {
+    rate = rsdData;
+  } else {
+    CURRENCY1.value === "RSD" && CURRENCY2.value === "USD";
+    rate = 1 / rsdData;
+  }
+
+  CONVERTED_AMOUNT.value = `${Math.ceil(amountValue * rate)} ${
+    CURRENCY2.value
+  }`;
+}
+
+AMOUNT_INPUT.addEventListener("input", updateConvertedAmount);
+CURRENCY1.addEventListener("change", updateConvertedAmount);
+CURRENCY2.addEventListener("change", updateConvertedAmount);
