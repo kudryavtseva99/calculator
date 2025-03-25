284f9529b5498bcf7eb946049b84a14b060bb3f1
kudryavtsevaa99 <kudryyaaa@yandex.ru>
2025-03-25 18:22:11 +0100

    rename the variable

--- exchange.js
+++ exchange.js
@@ -3,8 +3,8 @@ const url = `https://openexchangerates.org/api/latest.json?app_id=${apiKey}`;
 
 const AMOUNT_INPUT = document.querySelector(".amount");
 const CONVERTED_AMOUNT = document.querySelector(".converted-amount");
-const CURRENCY1 = document.querySelector(".currency1");
-const CURRENCY2 = document.querySelector(".currency2");
+const CURRENCY1_SELECTOR = document.querySelector(".currency1");
+const CURRENCY2_SELECTOR = document.querySelector(".currency2");
 const SWAP_BUTTON = document.querySelector(".swap-button");
 const CLEAR_BUTTON = document.querySelector(".clear-button");
 
@@ -16,8 +16,8 @@ let currencies = ["RSD", "USD", "EUR"];
 let currenciesList = {};
 
 AMOUNT_INPUT.addEventListener("input", updateConvertedAmount);
-CURRENCY1.addEventListener("change", updateConvertedAmount);
-CURRENCY2.addEventListener("change", updateConvertedAmount);
+CURRENCY1_SELECTOR.addEventListener("change", updateConvertedAmount);
+CURRENCY2_SELECTOR.addEventListener("change", updateConvertedAmount);
 SWAP_BUTTON.addEventListener("click", swapCurrencies);
 CLEAR_BUTTON.addEventListener("click", clearCurrencies);
 
@@ -42,12 +42,12 @@ function fillCurrencyOptions() {
     const option1 = document.createElement("option");
     option1.value = currency;
     option1.textContent = currency;
-    CURRENCY1.appendChild(option1);
+    CURRENCY1_SELECTOR.appendChild(option1);
 
     const option2 = document.createElement("option");
     option2.value = currency;
     option2.textContent = currency;
-    CURRENCY2.appendChild(option2);
+    CURRENCY2_SELECTOR.appendChild(option2);
   });
 }
 fillCurrencyOptions();
@@ -56,24 +56,26 @@ fillCurrencyOptions();
 function updateConvertedAmount() {
   const amountValue = parseFloat(AMOUNT_INPUT.value);
   if (isNaN(amountValue)) {
-    CONVERTED_AMOUNT.value = `0 ${CURRENCY2.value}`;
+    CONVERTED_AMOUNT.value = `0 ${CURRENCY2_SELECTOR.value}`;
     return;
   }
 
-  const exchangeRate1 = currenciesList[CURRENCY1.value];
-  const exchangeRate2 = currenciesList[CURRENCY2.value];
+  const exchangeRate1 = currenciesList[CURRENCY1_SELECTOR.value];
+  const exchangeRate2 = currenciesList[CURRENCY2_SELECTOR.value];
 
   if (exchangeRate1 && exchangeRate2) {
     const rateAmount = (amountValue / exchangeRate1) * exchangeRate2;
-    CONVERTED_AMOUNT.value = `${rateAmount.toFixed(2)} ${CURRENCY2.value}`;
+    CONVERTED_AMOUNT.value = `${rateAmount.toFixed(2)} ${
+      CURRENCY2_SELECTOR.value
+    }`;
   }
 }
 
 // функция для кнопки "смена местами валют"
 function swapCurrencies() {
-  const currentCurrency = CURRENCY1.value;
-  CURRENCY1.value = CURRENCY2.value;
-  CURRENCY2.value = currentCurrency;
+  const currentCurrency = CURRENCY1_SELECTOR.value;
+  CURRENCY1_SELECTOR.value = CURRENCY2_SELECTOR.value;
+  CURRENCY2_SELECTOR.value = currentCurrency;
   updateConvertedAmount();
 }
 
@@ -81,5 +83,5 @@ function swapCurrencies() {
 
 function clearCurrencies() {
   AMOUNT_INPUT.value = "";
-  CONVERTED_AMOUNT.value = `0 ${CURRENCY2.value}`;
+  CONVERTED_AMOUNT.value = `0 ${CURRENCY2_SELECTOR.value}`;
 }
